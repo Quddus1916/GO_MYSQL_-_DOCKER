f8ee0d303aedf0f39a31a0fcad15b395bf56555d
Quddus1916 <quddusjunior1916@gmail.com>
2022-05-10 23:32:40 -0500

    controller done

--- controller/homepagecontroller.go
+++ controller/homepagecontroller.go
@@ -1,9 +1,12 @@
 package controller
 
 import(
+	"github.com/labstack/echo"
+	"net/http"
 
 )
 
-func homePage (){
-	
+func homePage ( c echo.Context) error{
+     a:= "hello"
+	return c.JSON(http.StatusOK, a)
 }
--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import(
-	"fmt"
-	"net/http"
+	
 	"github.com/labstack/echo"
     "github.com/Quddus1916/GO_MYSQL_DOCKER/controller"
+	"github.com/labstack/echo/middleware"
 )
 
 func main(){
       e:= echo.New()
+	  e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: " uri=${uri},method=${method}, status=${status}\n",
+	  }))
 	  e.GET("/homepage",controller.homePage)
+
+	  e.Start(":8080")
+
 }
